c3af87a1de44fbf10c2acce32370cac3dacc0664
Jin-En Becerra <158262039+JinEnBecerra@users.noreply.github.com>
2026-02-25 22:15:49 -0800

    Fixed order_month sorting and optimized query using a subquery

--- sql_queries/01_monthly_revenue_trend.sql
+++ sql_queries/01_monthly_revenue_trend.sql
@@ -2,13 +2,23 @@
 -- Tables used: orders, payments
 -- Only analyzing orders that are completed (order_status = 'delivered')
 
-SELECT to_char(date_trunc('month', order_purchase_timestamp::timestamp ), 'YYYY-Mon') AS order_month_date, sum(payment_value) AS monthly_revenue
-FROM orders o
-INNER JOIN order_payments  p
-ON o.order_id = p.order_id
-WHERE o.order_status = 'delivered'
-GROUP BY 1
-ORDER BY 1
+
+SELECT 
+    to_char(order_month, 'YYYY-Mon') AS sorted_order_month, 
+    SUM(payment_value) AS monthly_revenue
+FROM (
+    SELECT 
+        date_trunc('month', order_purchase_timestamp::timestamp) AS order_month,
+        payment_value 
+    FROM orders o
+    INNER JOIN order_payments p
+        ON o.order_id = p.order_id
+    WHERE o.order_status = 'delivered'
+) t
+GROUP BY order_month
+ORDER BY order_month;
+
+
 
 
 
