4eb10086ade8fb70d929e9af126f7997d8281ee0
Jin-En Becerra <158262039+JinEnBecerra@users.noreply.github.com>
2026-02-27 19:08:27 -0800

    Added query logic

--- sql_queries/01_monthly_revenue_trend.sql
+++ sql_queries/01_monthly_revenue_trend.sql
@@ -1,7 +1,13 @@
--- Question: How has revenue changed over time?
--- Tables used: orders, payments
--- Only analyzing orders that are completed (order_status = 'delivered')
-
+/*
+Question: How has revenue changed over time?
+Tables used: orders, payments
+Logic: 
+- Only analyzing orders that are completed (order_status = 'delivered')
+- Cast order_purchase_timestamp to timestamp type
+- Inner join order_payments to sum payment values and get total revenue
+- Use date_trunc to truncate to month level, then to_char to format for display
+- Group and order by the truncated timestamp (not the formatted string) to ensure correct chronological ordering
+*/
 
 SELECT 
     to_char(order_month, 'YYYY-Mon') AS sorted_order_month, 
