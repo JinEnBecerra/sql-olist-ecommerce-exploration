a88fdba01f53b6fff30e58e9203da37fe0c9dc59
Jin-En Becerra <158262039+JinEnBecerra@users.noreply.github.com>
2026-03-02 22:31:59 -0800

    Added data cleaning and restructured the CTE
    Refactor SQL query to include data cleaning and filtering for average delivery time calculation.

--- sql_queries/03_average_delivery_time.sql
+++ sql_queries/03_average_delivery_time.sql
@@ -3,22 +3,35 @@ Question: How has average delivery time changed over time?
 Table: orders
 
 Logic:
+- Data cleaning 
 - Filter orders where order_status = 'delivered'
 - Calculate delivery time = order_delivered_customer_date - order_purchase_timestamp
 - Group by month
 - AVG(delivery time) to get monthly average
 - Filter out null/empty order_delivered_customer_date to avoid timestamp errors
+- Filter out unusually long delivery times (> 60 days) as they likely represent operational exceptions
 */
 
-WITH delivery_metrics AS (
+WITH clean_orders AS (
     SELECT
-        date_trunc('month', order_purchase_timestamp::timestamp ) AS purchase_month,
-        round(avg(date_part('day', order_delivered_customer_date::timestamp - order_purchase_timestamp::timestamp)::NUMERIC), 1) AS average_delivery_days
+        *
     FROM
         orders
     WHERE
         order_status = 'delivered'
+        AND order_delivered_customer_date IS NOT NULL
         AND order_delivered_customer_date != ''
+        AND order_purchase_timestamp IS NOT NULL
+        AND order_purchase_timestamp != ''
+        AND order_delivered_customer_date::timestamp >= order_purchase_timestamp::timestamp
+        AND order_delivered_customer_date::timestamp - order_purchase_timestamp::timestamp <= INTERVAL '60 days'
+),
+delivery_metrics AS (
+    SELECT
+        date_trunc('month', order_purchase_timestamp::timestamp ) AS purchase_month,
+        round(avg(date_part('day', order_delivered_customer_date::timestamp - order_purchase_timestamp::timestamp)::NUMERIC), 1) AS average_delivery_days
+    FROM
+        clean_orders
     GROUP BY
         1
 )
